5be4270a4f3e52162a017437db1833b38f1dc391
noah hetrick <noah.hetrick@onxmaps.com>
2025-06-20 10:37:13 -0400

    ex3

---
+++ ch3/ex3/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	type Employee struct {
+		firstName string
+		lastName  string
+		id        int
+	}
+
+	structOne := Employee{
+		"whitetail",
+		"deer",
+		1,
+	}
+
+	structTwo := Employee{
+		firstName: "ruffed",
+		lastName:  "grouse",
+		id:        2,
+	}
+
+	var structThree = Employee{
+		structTwo.firstName,
+		structOne.lastName,
+		3,
+	}
+
+	fmt.Println("employee 1:", structOne)
+	fmt.Println("employee 2:", structTwo)
+	employee3 := fmt.Sprintf("employee %d: %v\n", structThree.id, structThree)
+	fmt.Printf(employee3)
+}
