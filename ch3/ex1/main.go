1870245af15864cc58610b73444e3ea547e04a40
noah hetrick <noah.hetrick@onxmaps.com>
2025-06-20 09:57:55 -0400

    finish ex1

--- ch3/ex1/main.go
+++ ch3/ex1/main.go
@@ -6,7 +6,17 @@ func main() {
 	greetings := []string{
 		"Hello",
 		"Hola",
+		"Aloha",
 		"Howdy",
+		"Wazzup",
 	}
-	fmt.Println(greetings)
+
+	first_second := greetings[:2]
+	second_third_fourth := greetings[1:4]
+	fourth_fifth := greetings[3:5]
+
+	fmt.Println("          greetings:", greetings)
+	fmt.Println("       first_second:", first_second)
+	fmt.Println("second_third_fourth:", second_third_fourth)
+	fmt.Println("       fourth_fifth:", fourth_fifth)
 }
